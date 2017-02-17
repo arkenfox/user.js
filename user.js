1f7d5b3d2990b254730eeef24a6946d429a6fd99
Roman-Nopantski <simon.mainey@gmail.com>
2017-02-17 18:14:30 +1300

    Create user.js

---
+++ user.js
@@ -0,0 +1,3 @@
+can't paste in here, it loses all line breaks, and I am not manually adding 1750+ line breaks - here is an example paste
+
+/*** 0100: STARTUP ***/user_pref("ghacks_user.js.parrot", "0100 syntax error: the parrot's dead!");// 0101: disable "slow startup" options   // warnings, disk history, welcomes, intros, EULA, default browser checkuser_pref("browser.slowStartup.notificationDisabled", true);user_pref("browser.slowStartup.maxSamples", 0);
