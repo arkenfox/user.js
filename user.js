1b73cb828ebb5ad38b0aea5b41b7b6a780994413
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2017-05-10 01:39:02 +1200

    0201: geo.wifi.uri

--- user.js
+++ user.js
@@ -70,12 +70,12 @@ user_pref("browser.shell.checkDefaultBrowser", false);
 
 /*** 0200: GEOLOCATION ***/
 user_pref("ghacks_user.js.parrot", "0200 syntax error: the parrot's definitely deceased!");
-/* 0201: disable location-aware browsing ***/
+/* 0201: disable location-aware browsing, but enforce Mozilla's service over Google's ***/
 user_pref("geo.enabled", false);
-user_pref("geo.wifi.uri", "https://127.0.0.1");
+user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+user_pref("geo.wifi.xhr.timeout", 1); // reset this if you use geolocation
 user_pref("geo.wifi.logging.enabled", false); // (hidden pref)
 user_pref("browser.search.geoip.url", "");
-user_pref("geo.wifi.xhr.timeout", 1);
 user_pref("browser.search.geoip.timeout", 1);
 /* 0202: disable GeoIP-based search results
  * [NOTE] may not be hidden if Firefox has changed your settings due to your locale
