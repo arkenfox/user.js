9d6da7199dee7ca8aab5c0eeb350d1d1f8bd1884
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2017-05-14 23:17:16 +1200

    0401 better wording for extension.blocklist

--- user.js
+++ user.js
@@ -222,7 +222,7 @@ user_pref("dom.flyweb.enabled", false);
 ***/
 user_pref("ghacks_user.js.parrot", "0400 syntax error: the parrot's passed on!");
 /** BLOCKLISTS ***/
-/* 0401: enable extension blocklist, but sanitize blocklist url
+/* 0401: enable Firefox blocklist, but sanitize blocklist url
  * [NOTE] It includes updates for "revoked certificates"
  * [1] https://blog.mozilla.org/security/2015/03/03/revoking-intermediate-certificates-introducing-onecrl
  * [2] https://trac.torproject.org/projects/tor/ticket/16931 ***/
