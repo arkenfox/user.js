29ce8317745bb6dca35d964cbdaeeabfb6d5fb6c
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2017-08-05 09:07:09 +1200

    0201: remove mozilla API as it is now default 55+

--- user.js
+++ user.js
@@ -72,7 +72,7 @@ user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("ghacks_user.js.parrot", "0200 syntax error: the parrot's definitely deceased!");
 /* 0201: disable location-aware browsing, but enforce Mozilla's service over Google's ***/
 user_pref("geo.enabled", false);
-user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+user_pref("geo.wifi.uri", "");
 user_pref("geo.wifi.xhr.timeout", 1); // reset this if you use geolocation
 user_pref("geo.wifi.logging.enabled", false); // (hidden pref)
 user_pref("browser.search.geoip.url", "");
