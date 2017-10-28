d4cfd1e8854c460ca8c59b0ac4b20322f6ce6756
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2017-10-29 05:11:43 +1300

    4500: canvas extraction 967895

--- user.js
+++ user.js
@@ -1570,6 +1570,7 @@ user_pref("privacy.firstparty.isolate.restrict_opener_access", true);
  ** 1354633 - limit MediaError.message to a whitelist (FF57+)
  ** 1382533 - enable fingerprinting resistance for Presentation API (see 2513) (FF57+)
       This blocks exposure of local IP Addresses via mDNS (Multicast DNS)
+ **  967895 - enable site permission prompt before allowing canvas data extraction (FF58+)
 ***/
 user_pref("_user.js.parrot", "4500 syntax error: the parrot's popped 'is clogs");
 /* 4501: enable privacy.resistFingerprinting (FF41+)
