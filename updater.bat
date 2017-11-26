a269e53f88a27118bd49aacf89c19dbbba71b253
earthlng <earthlng@users.noreply.github.com>
2017-11-26 15:57:32 +0100

    Update updater.bat
    - multioverrides support
    - formatting changes

--- updater.bat
+++ updater.bat
@@ -3,54 +3,74 @@ TITLE ghacks user.js updater
 
 REM ### ghacks-user.js updater for Windows
 REM ## author: @claustromaniac
-REM ## version: 2.1
+REM ## version: 2.2
 
+SET _myname=%~n0
+SET _myparams=%*
 SETLOCAL EnableDelayedExpansion
-SET "_ua="
-SET "_log="
 :parse
-IF "%~1"=="" GOTO endparse
-IF "%~1"=="-unattended" SET "_ua=true"
-IF "%~1"=="-log" SET "_log=true"
+IF "%~1"=="" (
+	GOTO endparse
+)
+IF /I "%~1"=="-unattended" (
+	SET _ua=1
+)
+IF /I "%~1"=="-log" (
+	SET _log=1
+)
+IF /I "%~1"=="-multioverrides" (
+	SET _multi=1
+)
 SHIFT
 GOTO parse
 :endparse
-SET "_name="
-SET "_date="
-SET "_version="
+ECHO.
 SET /A "_line=0"
-ECHO.
-IF EXIST user.js (
-	FOR /F "delims=" %%i IN (user.js) DO (
-		IF !_line! EQU 1 SET "_name=%%i"
-		IF !_line! EQU 2 SET "_date=%%i"
-		IF !_line! EQU 3 SET "_version=%%i"
+IF NOT EXIST user.js (
+	ECHO user.js not detected in the current directory.
+) ELSE (
+	FOR /F "skip=1 tokens=1,2 delims=:" %%G IN (user.js) DO (
 		SET /A "_line+=1"
-		IF !_line! GEQ 4 GOTO break
+		IF !_line! GEQ 4 (
+			GOTO exitloop
+		)
+		IF !_line! EQU 1 (
+			SET _name=%%H
+		)
+		IF !_line! EQU 2 (
+			SET _date=%%H
+		)
+		IF !_line! EQU 3 (
+			SET _version=%%G
+		)
 	)
-	:break
+	:exitloop
 	IF !_line! GEQ 4 (
-		IF "ghacks"=="!_name:~8,6!" (
-			FOR /F "delims=:" %%G IN ("!_version!") DO SET "_version=%%G"
-			SET "_version=!_version:~2!"
-			SET "_date=!_date:~8!"
-			ECHO ghacks user.js !_version!, !_date!
-		) ELSE ( ECHO Current user.js version not recognised. )
-	) ELSE ( ECHO Current user.js version not recognised. )
-) ELSE ( ECHO user.js not detected in the current directory. )
+		IF /I NOT "!_name!"=="!_name:ghacks=X!" (
+			ECHO ghacks user.js !_version:~2!,!_date!
+		) ELSE (
+			ECHO Current user.js version not recognised.
+		)
+	) ELSE (
+		ECHO Current user.js version not recognised.
+	)
+)
 ECHO.
-IF NOT "%_ua%"=="true" (
+IF NOT DEFINED _ua (
+	ECHO.
 	ECHO This batch should be run from your Firefox profile directory. It will download the latest version of ghacks user.js from github and then append any of your own changes from user-overrides.js to it.
 	ECHO.
 	REM ECHO Visit the wiki for more detailed information.
 	REM ECHO.
 	CHOICE /M "Continue"
-	IF ERRORLEVEL 2 GOTO end
+	IF ERRORLEVEL 2 (
+		GOTO end
+	)
 )
 CLS
 ECHO.
-IF "%_log%"=="true" (
-	CALL :log >>user.js-update-log.txt 2>&1 
+IF DEFINED _log (
+	CALL :log >>user.js-update-log.txt 2>&1
 	EXIT /B
 	:log
 	ECHO ##################################################################
@@ -59,49 +79,83 @@ IF "%_log%"=="true" (
 	ECHO.
 )
 IF EXIST user.js (
-	IF EXIST user.js.bak REN user.js.bak user.js.old.bak
+	IF EXIST user.js.bak (
+		REN user.js.bak user.js.old.bak
+	)
 	REN user.js user.js.bak
 	ECHO Current user.js file backed up.
 	ECHO.
 )
-ECHO Retrieving latest user.js file from ghacks github repository...
+ECHO Retrieving latest user.js file from github repository...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/ghacksuserjs/ghacks-user.js/raw/master/user.js', 'user.js')" >nul
 ECHO.
 IF EXIST user.js (
-	IF EXIST "user-overrides.js" (
-		ECHO Appending user-overrides.js...
+	IF DEFINED _multi (
+		ECHO Multiple overrides enabled. List of files found:
+		FORFILES /P user.js-overrides /M *.js
+		IF %ERRORLEVEL% EQU 0 (
+			IF DEFINED _merge (
+				ECHO.
+				ECHO Merging not supported yet...
+				ECHO.
+			) ELSE (
+				ECHO.
+				ECHO Appending...
+				ECHO.
+				COPY /B /V /Y user.js+"user.js-overrides\*.js" user.js
+			)
+		)
 		ECHO.
-		COPY /B /V /Y user.js+"user-overrides.js" "tempuserjs"
-		DEL /F user.js
-		REN tempuserjs user.js
+	) ELSE (
+		IF EXIST "user-overrides.js" (
+			IF DEFINED _merge (
+				ECHO Merging user-overrides.js not supported yet...
+			) ELSE (
+				ECHO Appending user-overrides.js...
+				ECHO.
+				COPY /B /V /Y user.js+"user-overrides.js" "user.js"
+			)
+		) ELSE (
+			ECHO user-overrides.js not found.
+		)
 		ECHO.
 	)
 	ECHO Handling backups...
 	SET "changed="
-	IF EXIST user.js.bak ( FC user.js.bak user.js >nul && SET "changed=false" || SET "changed=true" )
+	IF EXIST user.js.bak (
+		FC user.js.bak user.js >nul && SET "changed=false" || SET "changed=true"
+	)
 	ECHO.
 	ECHO.
 	IF "!changed!"=="true" (
-		IF EXIST user.js.old.bak DEL /F user.js.old.bak
+		IF EXIST user.js.old.bak (
+			DEL /F user.js.old.bak
+		)
 		ECHO Update complete.
 	) ELSE (
 		IF "!changed!"=="false" (
 			DEL /F user.js.bak
 			IF EXIST user.js.old.bak REN user.js.old.bak user.js.bak
 			ECHO Update completed without changes.
-		) ELSE ECHO Update complete.
+		) ELSE (
+			ECHO Update complete.
+		)
 	)
 	ECHO.
 ) ELSE (
-	IF EXIST user.js.bak REN user.js.bak user.js
-	IF EXIST user.js.old.bak REN user.js.old.bak user.js.bak
+	IF EXIST user.js.bak (
+		REN user.js.bak user.js
+	)
+	IF EXIST user.js.old.bak (
+		REN user.js.old.bak user.js.bak
+	)
 	ECHO.
 	ECHO Update failed. Make sure PowerShell is allowed internet access.
 	ECHO.
 	ECHO No changes were made.
 	ECHO.
 )
-IF NOT "%_log%"=="true" (
-        IF NOT "%_ua%"=="true" PAUSE
+IF NOT DEFINED _log (
+	IF NOT DEFINED _ua PAUSE
 )
 :end
