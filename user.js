5385b8c4abc98647dad38e6cfbe03cde1609bf7a
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-02-19 12:24:28 +0000

    remove 5017: ui.submenuDelay #360 #337

--- user.js
+++ user.js
@@ -1746,9 +1746,6 @@ user_pref("network.manage-offline-status", false);
    // user_pref("toolkit.cosmeticAnimations.enabled", false);
 /* 5016: disable reload/stop animation (FF56+) ***/
    // user_pref("browser.stopReloadAnimation.enabled", true);
-/* 5017: set submenu delay in milliseconds. 0=instant while a small number allows
- * a mouse pass over menu items without any submenus alarmingly shooting out ***/
-user_pref("ui.submenuDelay", 150); // (hidden pref)
 /* 5018: set maximum number of daily bookmark backups to keep (default is 15) ***/
 user_pref("browser.bookmarks.max_backups", 2);
 /* 5020: control urlbar click behaviour (with defaults) ***/
