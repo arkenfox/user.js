a2b5e1e7cfec152a94d3ec869591246b2cd2b47a
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-06-04 00:28:43 +0000

    updated 03-Jun-2018

--- scratchpad-scripts/ghacks-clear-[removed].js
+++ scratchpad-scripts/ghacks-clear-[removed].js
@@ -1,7 +1,7 @@
 /***
  This will reset the preferences that have been removed completely from the ghacks user.js.
 
- Last updated: 27-May-2018
+ Last updated: 03-Jun-2018
 
  For instructions see:
  https://github.com/ghacksuserjs/ghacks-user.js/wiki/3.1-Resetting-Inactive-Prefs-[Scripts]
