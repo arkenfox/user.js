2969c6660f0bc98f2f20f889554836ff0063ae2a
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-08-16 14:36:48 +0000

    removed browser.urlbar.autoFill.typed

--- scratchpad-scripts/ghacks-clear-[removed].js
+++ scratchpad-scripts/ghacks-clear-[removed].js
@@ -1,7 +1,7 @@
 /***
  This will reset the preferences that have been removed completely from the ghacks user.js.
 
- Last updated: 06-July-2018
+ Last updated: 16-August-2018
 
  For instructions see:
  https://github.com/ghacksuserjs/ghacks-user.js/wiki/3.1-Resetting-Inactive-Prefs-[Scripts]
@@ -99,6 +99,8 @@
     'network.http.fast-fallback-to-IPv4',
     'offline-apps.quota.warn',
     'services.blocklist.signing.enforced',
+    /* 62-beta */
+    'browser.urlbar.autoFill.typed',
     /* reset parrot: check your open about:config after running the script */
     '_user.js.parrot'
   ]
