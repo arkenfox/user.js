24f6f454a8a624962a477309623d2e41f0f332b4
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-02-04 06:08:44 +0000

    cleanup 2701 #628
    put the "What to block" bit in the [SETTING] and correct the dropdown titles in the options (minus the *may break website* bits)

--- user.js
+++ user.js
@@ -1401,13 +1401,11 @@ user_pref("security.dialog_enable_delay", 700);
      accessible to websites except shared/service workers where the cookie setting *must* be "Allow"
 ***/
 user_pref("_user.js.parrot", "2700 syntax error: the parrot's joined the bleedin' choir invisible!");
-/* 2701: disable 3rd-party cookies and site-data
- * You can set exceptions under site permissions or use an extension
- * 0=Accept cookies and site data, 1=Block third-party cookies, 2=Block all cookies,
- * 3=Block cookies from unvisited sites, 4=Block third-party trackers (FF63+)
- * [NOTE] value 4 is tied to the Tracking Protection lists so make sure you have 0424 + 0425 on default values!
- * [SETTING] Privacy & Security>Content Blocking>Custom>Cookies
- * [1] https://www.fxsitecompat.com/en-CA/docs/2015/web-storage-indexeddb-cache-api-now-obey-third-party-cookies-preference/ ***/
+/* 2701: disable 3rd-party cookies
+ * 0=default (Standard Content Blocking / Custom Content Blocking with "Cookies" unchecked)
+ * 1=Third-party trackers, 2=All cookies 3=Cookies from unvisited sites, 4=All third-party trackers (FF63+)
+ * [NOTE] You can set exceptions under site permissions or use an extension
+ * [SETTING] Privacy & Security>Content Blocking>Custom>Choose what to block>Cookies ***/
 user_pref("network.cookie.cookieBehavior", 1);
 /* 2702: set third-party cookies (i.e ALL) (if enabled, see 2701) to session-only
    and (FF58+) set third-party non-secure (i.e HTTP) cookies to session-only
