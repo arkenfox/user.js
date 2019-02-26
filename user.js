eae84348536e4ffd9f411c8ef95d0c91e2387365
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-02-26 07:53:21 +0000

    start 66 commits

--- user.js
+++ user.js
@@ -1,8 +1,8 @@
 /******
 * name: ghacks user.js
 * date: 26 February 2019
-* version 65: Dancing with My Pants
-*   "If I had the chance, I'd ask the world to dance, and I'll be dancing with my pants"
+* version 66-alpha: The Power of Pants
+*   "The power of pants is a curious thing. Make a one man weep, make another man sing"
 * authors: v52+ github | v51- www.ghacks.net
 * url: https://github.com/ghacksuserjs/ghacks-user.js
 * license: MIT: https://github.com/ghacksuserjs/ghacks-user.js/blob/master/LICENSE.txt
