e3349d0f076de90b70dfe75c78cfe697a71d12e6
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-04-15 14:17:52 +0000

    2212 defaults [67+ change]

--- user.js
+++ user.js
@@ -1123,7 +1123,7 @@ user_pref("dom.disable_open_during_load", true);
 /* 2211: set max popups from a single non-click event - default is 20! ***/
 user_pref("dom.popup_maximum", 3);
 /* 2212: limit events that can cause a popup
- * default is "change click dblclick mouseup pointerup notificationclick reset submit touchend"
+ * default is "change click dblclick mouseup pointerup notificationclick reset submit touchend contextmenu"
  * [1] http://kb.mozillazine.org/Dom.popup_allowed_events ***/
 user_pref("dom.popup_allowed_events", "click dblclick");
 
