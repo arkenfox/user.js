b05cfc32de098f7bf9ead0b4bd079def38df80ba
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-04-23 04:04:22 +0000

    HTTP2 tweak

--- user.js
+++ user.js
@@ -474,14 +474,14 @@ user_pref("_user.js.parrot", "0700 syntax error: the parrot's given up the ghost
  * [2] https://www.internetsociety.org/tag/ipv6-security/ (see Myths 2,4,5,6) ***/
 user_pref("network.dns.disableIPv6", true);
 /* 0702: disable HTTP2
- * HTTP2 raises some concerns with "multiplexing" and "server push", does nothing to
- * enhance privacy, and may open up a number of server-side fingerprinting opportunities.
+ * HTTP2 raises concerns with "multiplexing" and "server push", does nothing to
+ * enhance privacy, and opens up a number of server-side fingerprinting opportunities.
  * [WARNING] Disabling this made sense in the past, and doesn't break anything, but HTTP2 is
- * now at 35% and growing, see [5] - don't be that one person using HTTP1.1 on HTTP2 sites
+ * at 35% (April 2019) and growing [5]. Don't be that one person using HTTP1.1 on HTTP2 sites
  * [1] https://http2.github.io/faq/
  * [2] https://blog.scottlogic.com/2014/11/07/http-2-a-quick-look.html
- * [3] https://queue.acm.org/detail.cfm?id=2716278
- * [4] https://github.com/ghacksuserjs/ghacks-user.js/issues/107
+ * [3] https://http2.github.io/http2-spec/#rfc.section.10.8
+ * [4] https://queue.acm.org/detail.cfm?id=2716278
  * [5] https://w3techs.com/technologies/details/ce-http2/all/all ***/
    // user_pref("network.http.spdy.enabled", false);
    // user_pref("network.http.spdy.enabled.deps", false);
