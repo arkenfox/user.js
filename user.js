27bd07d496f00247c49c140a13717b15f2324635
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-06-28 04:22:50 +0000

    5000s: userChrome / userContent

--- user.js
+++ user.js
@@ -1621,6 +1621,7 @@ user_pref("_user.js.parrot", "5000 syntax error: this is an ex-parrot!");
 /* APPEARANCE ***/
    // user_pref("browser.download.autohideButton", false); // [FF57+]
    // user_pref("toolkit.cosmeticAnimations.enabled", false); // [FF55+]
+   // user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
 /* CONTENT BEHAVIOR ***/
    // user_pref("accessibility.typeaheadfind", true); // enable "Find As You Type"
    // user_pref("clipboard.autocopy", false); // disable autocopy default [LINUX]
