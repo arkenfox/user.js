755ebf88fb99d2e42b4cb2f488d99dd7eec6d796
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-09-01 06:27:24 +0000

    start 69 commits

--- user.js
+++ user.js
@@ -1,8 +1,8 @@
 /******
 * name: ghacks user.js
-* date: 31 August 2019
-* version 68: Knock on Pants
-*   "It's like thunder, lightning... the way you wear me is frightening"
+* date: 1 September 2019
+* version 69-beta: Pants One More Time
+*   "When I'm not with pants I lose my mind. Give me a sign. Hit me, pants, one more time."
 * authors: v52+ github | v51- www.ghacks.net
 * url: https://github.com/ghacksuserjs/ghacks-user.js
 * license: MIT: https://github.com/ghacksuserjs/ghacks-user.js/blob/master/LICENSE.txt
