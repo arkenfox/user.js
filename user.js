f67e72919773ff51e712c515988c61e4d0a3d0e4
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-11-19 06:39:08 +0000

    whatsNewPanel correct version

--- user.js
+++ user.js
@@ -1622,7 +1622,7 @@ user_pref("_user.js.parrot", "5000 syntax error: this is an ex-parrot!");
    // user_pref("ui.key.menuAccessKey", 0); // disable alt key toggling the menu bar [RESTART]
    // user_pref("view_source.tab", false); // view "page/selection source" in a new window [FF68+, FF59 and under]
 /* UX: FEATURES: disable and hide the icons and menus ***/
-   // user_pref("browser.messaging-system.whatsNewPanel.enabled", false); // What's New [FF70+]
+   // user_pref("browser.messaging-system.whatsNewPanel.enabled", false); // What's New [FF69+]
    // user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
    // user_pref("identity.fxaccounts.enabled", false); // Firefox Accounts & Sync [FF60+] [RESTART]
    // user_pref("reader.parse-on-load.enabled", false); // Reader View
