5b1d56933ba921ea0fee85fd46e355c484d70b87
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-12-19 16:21:21 +0000

    middlemouse.paste, see #735

--- scratchpad-scripts/ghacks-clear-[removed].js
+++ scratchpad-scripts/ghacks-clear-[removed].js
@@ -1,7 +1,7 @@
 /***
  This will reset the preferences that have been removed completely from the ghacks user.js.
 
- Last updated: 11-December-2019
+ Last updated: 19-December-2019
 
  For instructions see:
  https://github.com/ghacksuserjs/ghacks-user.js/wiki/3.1-Resetting-Inactive-Prefs-[Scripts]
@@ -217,6 +217,7 @@
     'webgl.dxgl.enabled',
     /* 71-beta */
     'media.block-autoplay-until-in-foreground',
+    'middlemouse.paste',
     /* reset parrot: check your open about:config after running the script */
     '_user.js.parrot'
   ]
