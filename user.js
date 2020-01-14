e1022c2e72010c6d7b3db458c7b1c7e6f1193879
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-01-14 17:38:22 +0000

    72-beta

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 /******
 * name: ghacks user.js
-* date: 19 December 2019
-* version 72-alpha
+* date: 20 January 2020
+* version 72-beta
 * authors: v52+ github | v51- www.ghacks.net
 * url: https://github.com/ghacksuserjs/ghacks-user.js
 * license: MIT: https://github.com/ghacksuserjs/ghacks-user.js/blob/master/LICENSE.txt
