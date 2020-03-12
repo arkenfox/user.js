b6e2a3f64f030fa3586984f294931d3d0334cfbd
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-03-12 14:44:14 +0000

    one of the 2012 webgl prefs deprecated

--- user.js
+++ user.js
@@ -918,7 +918,6 @@ user_pref("webgl.disabled", true);
 user_pref("webgl.enable-webgl2", false);
 /* 2012: limit WebGL ***/
 user_pref("webgl.min_capability_mode", true);
-user_pref("webgl.disable-extensions", true);
 user_pref("webgl.disable-fail-if-major-performance-caveat", true);
 /* 2022: disable screensharing ***/
 user_pref("media.getusermedia.screensharing.enabled", false);
@@ -1701,6 +1700,9 @@ user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?ke
    // [1] https://bugzilla.mozilla.org/1328756
    // [-] https://bugzilla.mozilla.org/1606265
 user_pref("privacy.userContext.longPressBehavior", 2);
+// 2012: limit WebGL
+   // [-] https://bugzilla.mozilla.org/1477756
+user_pref("webgl.disable-extensions", true);
 // * * * /
 // ***/
 
