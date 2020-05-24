868882ae3321c66bf979e455b0ab4731bdb22f71
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-24 18:11:55 +0000

    start 77-alpha

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 /******
 * name: ghacks user.js
 * date: 24 May 2020
-* version 76
+* version 77-alpha
 * authors: v52+ github | v51- www.ghacks.net
 * url: https://github.com/ghacksuserjs/ghacks-user.js
 * license: MIT: https://github.com/ghacksuserjs/ghacks-user.js/blob/master/LICENSE.txt
