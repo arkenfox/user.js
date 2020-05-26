21b6a1a732b8284473efe3ab4ff6835930501d8a
earthlng <earthlng@users.noreply.github.com>
2020-05-26 11:49:53 +0000

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -2,7 +2,7 @@
 
 ## ghacks-user.js updater for macOS and Linux
 
-## version: 2.5
+## version: 2.6
 ## Author: Pat Johnson (@overdodactyl)
 ## Additional contributors: @earthlng, @ema-pe, @claustromaniac
 
