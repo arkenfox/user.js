85f20097e4d588d21adc927ac4a8b0468eb413f6
TotallyLeGIT <bbkqx24kxlgvgbss@mailban.de>
2020-08-27 23:02:07 +0200

    bump version

--- updater.sh
+++ updater.sh
@@ -2,7 +2,7 @@
 
 ## ghacks-user.js updater for macOS and Linux
 
-## version: 2.6
+## version: 2.7
 ## Author: Pat Johnson (@overdodactyl)
 ## Additional contributors: @earthlng, @ema-pe, @claustromaniac
 
