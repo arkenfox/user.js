cfaf354fe359eee6bdc219c4b75823b94f77d2c0
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2021-02-02 04:09:50 +0000

    oophs, better start 86-alpha

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 /******
 * name: arkenfox user.js
-* date: 28 Jan 2021
-* version 85
+* date: 01 Feb 2021
+* version 86-alpha
 * url: https://github.com/arkenfox/user.js
 * license: MIT: https://github.com/arkenfox/user.js/blob/master/LICENSE.txt
 
