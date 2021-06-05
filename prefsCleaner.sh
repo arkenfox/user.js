25bfbf9c23ea6856c699a1eb90e89c421598fe2c
4n0nym0u53 <anonymous71@protonmail.com>
2021-06-05 20:04:29 +0200

    Bump version in infobox.

--- prefsCleaner.sh
+++ prefsCleaner.sh
@@ -39,7 +39,7 @@ fPrint() {
 	echo "                   ╔══════════════════════════╗"
 	echo "                   ║     prefs.js cleaner     ║"
 	echo "                   ║    by claustromaniac     ║"
-	echo "                   ║           v1.3           ║"
+	echo "                   ║           v1.4           ║"
 	echo "                   ╚══════════════════════════╝"
 	echo -e "\nThis script should be run from your Firefox profile directory.\n"
 	echo "It will remove any entries from prefs.js that also exist in user.js."
