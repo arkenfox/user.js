f1831e947bae0940bf8525447345c96ca27924ea
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2023-08-26 06:45:37 +0000

    media.gmp-widevinecdm.enabled
    this adds nothing, users can use `media.eme.enabled` if this is their threat model

--- scratchpad-scripts/arkenfox-cleanup.js
+++ scratchpad-scripts/arkenfox-cleanup.js
@@ -3,7 +3,7 @@
   - removed from the arkenfox user.js
   - deprecated by Mozilla but listed in the arkenfox user.js in the past
 
-  Last updated: 14-August-2023
+  Last updated: 26-August-2023
 
   Instructions:
   - [optional] close Firefox and backup your profile
@@ -242,6 +242,7 @@
 
     /* REMOVED */
     /* 116+ */
+    'media.gmp-widevinecdm.enabled',
     'network.protocol-handler.external.ms-windows-store',
     /* 103-115 */
     'beacon.enabled',
