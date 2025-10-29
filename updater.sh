5ba496ca2183439bfff3434f59284c789c003c40
luca0N! <luca0n@luca0n.com>
2025-10-28 23:13:22 -0300

    Fail curl request on HTTP error status
    Use the `-f' option to instruct curl to exit with a non-zero exit code
    if the HTTP response indicates an error via its status code.  This is
    the default behavior for wget, but curl will send the request and exit
    normally (with exit code equals to zero), as long as it's received a
    response from the server.

--- updater.sh
+++ updater.sh
@@ -48,7 +48,7 @@ ESR=false
 # Download method priority: curl -> wget
 DOWNLOAD_METHOD=''
 if command -v curl >/dev/null; then
-  DOWNLOAD_METHOD='curl --max-redirs 3 -so'
+  DOWNLOAD_METHOD='curl --max-redirs 3 -fso'
 elif command -v wget >/dev/null; then
   DOWNLOAD_METHOD='wget --max-redirect 3 --quiet -O'
 else
